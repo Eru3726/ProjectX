02d50cd8e39ad31013c13ed12448cdc74e978707
Eru <eru.gamecreater@gmail.com>
2023-12-20 11:24:30 +0900

    アセットアップデート及びプラグイン追加

---
+++ ProjectX/Assets/Eru/Scripts/LastBoss/ElisManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElisManager : MonoBehaviour
+{
+    //Neutral
+    //Move
+    //Shot
+    //Avatar
+    //FallingAttack
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
