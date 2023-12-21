9ca52f5b0234945be9312b5c855dcc5c623e8d45
Eru <eru.gamecreater@gmail.com>
2023-12-21 14:20:53 +0900

    少しだけ

--- ProjectX/Assets/Eru/Scripts/LastBoss/Data/ElisData.cs
+++ ProjectX/Assets/Eru/Scripts/LastBoss/Data/ElisData.cs
@@ -1,8 +1,11 @@
 [System.Serializable]
 public class ElisData
 {
-    public int hp;
-    public int attackPower;
+    public int hitPoint;
+    public int fallingAttackPower;
     public int defensePower;
-    public int speed;
+    public float moveSpeed;
+    public int b_AttackPower;
+    public float b_MoveSpeed;
+    public int b_MoveTime;
 }
--- ProjectX/Assets/Eru/Scripts/LastBoss/ElisManager.cs
+++ ProjectX/Assets/Eru/Scripts/LastBoss/ElisManager.cs
@@ -1,28 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum Elis_MoveType
+{
+    Entry = 0,      //0.登場
+    Neutral,        //1.待機状態
+    Move,           //2.移動
+    Shot,           //3.魔法弾攻撃
+    Avatar,         //4.分裂
+    FallingAttack,  //5.落下攻撃
+    FormChange,     //6.形態変化
+}
 
 public class ElisManager : MonoBehaviour
 {
-    //Neutral
-    //Move
-    //Shot
-    //Avatar
-    //FallingAttack
-
-    [SerializeField]
+    [SerializeField,Tooltip("ボスデータ")]
     private LastBossData lastBossData;
 
-    void Start()
-    {
-        Debug.Log(lastBossData.ElisFastData[0].hp);
-        Debug.Log(lastBossData.ElisFastData[0].attackPower);
-        Debug.Log(lastBossData.ElisFastData[0].defensePower);
-        Debug.Log(lastBossData.ElisFastData[0].speed);
+    private Elis_MoveType moveType;
 
-        Debug.Log(lastBossData.ElisSecondData[0].hp);
-        Debug.Log(lastBossData.ElisSecondData[0].attackPower);
-        Debug.Log(lastBossData.ElisSecondData[0].defensePower);
-        Debug.Log(lastBossData.ElisSecondData[0].speed);
+    private Rigidbody2D rb;
+
+    private int hp, attackPow, defensePow;
+
+    private float moveSpeed;
+
+
+    void Awake()
+    {
+        Debug.Log(lastBossData.ElisFastData[0].fallingAttackPower);
+        Debug.Log(lastBossData.ElisFastData[0].b_MoveTime);
+
+        rb = GetComponent<Rigidbody2D>();
+
+        hp = lastBossData.ElisFastData[0].hitPoint;
+        attackPow = lastBossData.ElisFastData[0].fallingAttackPower;
+        defensePow = lastBossData.ElisFastData[0].defensePower;
+        moveSpeed = lastBossData.ElisFastData[0].moveSpeed;
+
+        moveType = Elis_MoveType.Entry;
+        rb.gravityScale = 0;
     }
 }
